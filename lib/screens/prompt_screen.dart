d573877c8ce2cf5ed53604ccd7799c3eac4e7d86
omar mahmoud <omarmahmoudazema@gmail.com>
2024-11-18 22:32:13 +0200

    make the text and field icon in prompt screen

--- lib/screens/prompt_screen.dart
+++ lib/screens/prompt_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PromptScreen extends StatefulWidget {
   final VoidCallback ShowHomeScreen;
@@ -11,8 +13,52 @@ class PromptScreen extends StatefulWidget {
 class _PromptScreenState extends State<PromptScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('hello')),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 50.0, left: 16.0, right: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                      color: const Color(0xFF6D1B7B).withOpacity(0.9),
+                      width: 0.3),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //this is the text
+                  Text(
+                    "Text Translation",
+                    style: GoogleFonts.poppins(
+                        color: const Color(0xFF000000),
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  // this is the fields icon
+                  const Icon(
+                    Icons.text_fields,
+                    color: Color(0xFF000000),
+                    size: 24.0,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
